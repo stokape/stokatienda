71b4b8fa60b0cb2e4f669c0b844bf7531b3f2948
Xavi <stoka.peru@gmail.com>
2026-09-09 16:13:28 -0500

    Updates

--- assets/authStore-DXRqYHZJ.js
+++
@@ -1,1 +0,0 @@
-import{o as e,s as t}from"./jsx-runtime-BcOoUto4.js";var n=t()(e(e=>({currentUser:null,login:t=>e({currentUser:t}),logout:()=>e({currentUser:null})}),{name:`stoka-auth`}));export{n as t};
